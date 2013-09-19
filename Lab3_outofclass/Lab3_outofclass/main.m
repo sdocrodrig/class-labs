419820250943339d386c006fe026db666ddefe77
Danielle Rodriguez <Stephanie.Rodriguez@colorado.edu>
2013-09-19 09:20:18 -0600

    Lab 3 Rebuilt
    Rebuilt version of lab 3 due today

---
+++ Lab3_outofclass/Lab3_outofclass/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Lab3_outofclass
+//
+//  Created by new user on 9/19/13.
+//  Copyright (c) 2013 ATLS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MADAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MADAppDelegate class]));
+    }
+}
