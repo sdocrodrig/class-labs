9183ea9785a7cab3896cb3634c395da4a2d47439
Danielle Rodriguez <Stephanie.Rodriguez@colorado.edu>
2013-09-25 09:10:35 -0600

    Lab Tip Calculator - some error
    Simulator is distorting presentation of UI. Tried changing hardware
    device and also (suggested in Beginning iOS 6 Development) pinning top
    space to superview for all elements in view (pg. 99).

---
+++ tipCalculator_OutOfClass/tipCalculator_OutOfClass/MADViewController.h
@@ -0,0 +1,26 @@
+//
+//  MADViewController.h
+//  tipCalculator_OutOfClass
+//
+//  Created by new user on 9/24/13.
+//  Copyright (c) 2013 ATLS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MADViewController : UIViewController <UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *billAmount;
+@property (weak, nonatomic) IBOutlet UILabel *totalPeople;
+@property (weak, nonatomic) IBOutlet UILabel *totalPerPerson;
+@property (weak, nonatomic) IBOutlet UITextField *modifiedTip;
+@property (weak, nonatomic) IBOutlet UILabel *TipDetermined;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
+- (IBAction)gradeService:(UISegmentedControl *)sender;
+
+- (IBAction)changeNumberPeople:(UISlider *)sender;
+
+- (IBAction)calculateTip:(UIButton *)sender;
+
+
+@end
+;
